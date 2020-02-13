e9b48c364fe3bc34602c0bb65843631dffe1c2db
aonomike <aonomike@gmail.com>
2020-02-13 22:59:40 +0300

    Refactoring

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,9 @@
 class Movie < ApplicationRecord
+
+  def self.released
+    where("released_on < ?", Time.now).order("released_on")
+  end
+
   def flop?
     total_gross.blank? || total_gross < 243_000_000
   end
